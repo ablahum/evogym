aa74f9589c8c88a1005ec17434e0c470fbac9a10
ablahum <mriztama@gmail.com>
2023-06-15 23:17:42 +0700

    refacto navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import Navbar from '@/scenes/Navbar'
+import { useEffect, useState } from 'react'
+import { SelectedPage } from './shared/types'
+
 function App() {
-  return <></>
+  const [selectedPage, setselectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true)
+        setselectedPage(SelectedPage.Home)
+      }
+
+      if (window.scrollY !== 0) setIsTopOfPage(false)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <div className='app bg-gray-20'>
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setselectedPage={setselectedPage}
+      />
+    </div>
+  )
 }
 
 export default App
