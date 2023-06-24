87188e6a0409ee86e2a4b0904b0cd1488aef4985
ablahum <mriztama@gmail.com>
2023-06-24 23:47:09 +0700

    refacto

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,6 +5,7 @@ import { SelectedPage } from '@/shared/types'
 import useMediaQuery from '@/hooks/useMediaQuery'
 import { useState } from 'react'
 import ActionButton from '@/shared/ActionButton'
+import ResponsiveNav from './ResponsiveNav'
 
 type Props = {
   selectedPage: SelectedPage
@@ -73,39 +74,12 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
       </div>
 
       {!isAboveMediumScreens && isMenuToggled && (
-        <div className='fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'>
-          <div className='flex justify-end p-12'>
-            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-              <XMarkIcon className='h-6 w-6 text-gray-400' />
-            </button>
-          </div>
-
-          <div className='ml-[33%] flex flex-col gap-10 text-2xl'>
-            <Link
-              page='Home'
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-
-            <Link
-              page='Benefits'
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-
-            <Link
-              page='Our Classes'
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-
-            <Link
-              page='Contact Us'
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-          </div>
-        </div>
+        <ResponsiveNav
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+          isMenuToggled={isMenuToggled}
+          setIsMenuToggled={setIsMenuToggled}
+        />
       )}
     </div>
   )
