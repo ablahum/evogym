972e710f060321e03b13e18e7842005a2aa867ac
ablahum <mriztama@gmail.com>
2023-06-25 22:24:50 +0700

    refacto

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -7,16 +7,14 @@ type Props = {
   setSelectedPage: (value: SelectedPage) => void
 }
 
-const ActionButton = ({ children, setSelectedPage }: Props) => {
-  return (
-    <AnchorLink
-      className='rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white'
-      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-      href={`#${SelectedPage.ContactUs}`}
-    >
-      {children}
-    </AnchorLink>
-  )
-}
+const ActionButton = ({ children, setSelectedPage }: Props) => (
+  <AnchorLink
+    className='rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white'
+    onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+    href={`#${SelectedPage.ContactUs}`}
+  >
+    {children}
+  </AnchorLink>
+)
 
 export default ActionButton
