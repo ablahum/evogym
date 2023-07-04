790e60e66847c12a7c80b5a6f08f066e2ad18d7d
ablahum <mriztama@gmail.com>
2023-07-04 13:12:30 +0700

    refacto

--- src/components/Benefits/index.tsx
+++ src/components/Benefits/index.tsx
@@ -3,6 +3,8 @@ import { BenefitType, SelectedPage } from '@/shared/types'
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from '@heroicons/react/24/solid'
 import { motion } from 'framer-motion'
 import Benefit from './Benefit'
+import ActionButton from '@/shared/ActionButton'
+import BenefitsPageGraphic from '@/assets/BenefitsPageGraphic.png'
 
 const benefits: Array<BenefitType> = [
   {
@@ -73,6 +75,59 @@ const Benefits = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        <div className='mt-16 items-center justify-between gap-20 md:mt-28 md:flex'>
+          <img
+            src={BenefitsPageGraphic}
+            alt='benefits-page-graphic'
+            className='mx-auto'
+          />
+
+          <div className=''>
+            <motion.div
+              initial='hidden'
+              whileInView='visible'
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves'>
+                <HText>
+                  MILLIONS OF HAPPY MEMBERS GETTING HAPPY <span className='text-primary-500'>FIT</span>
+                </HText>
+              </div>
+            </motion.div>
+
+            <motion.div
+              initial='hidden'
+              whileInView='visible'
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className='my-5'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit illum impedit repudiandae vitae consequuntur omnis, culpa eum repellat odit dignissimos minus cum laboriosam mollitia quasi ea quibusdam distinctio nemo!
+                Adipisci dolorem commodi enim consequuntur ea amet corporis in exercitationem unde sapiente quas repudiandae veniam, sequi dolores omnis? Saepe, reiciendis nisi.
+              </p>
+
+              <p className='mb-5'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo aliquid non voluptatibus reprehenderit ab repellendus dolor eos ipsa, corporis, vitae, natus qui. Maxime obcaecati esse ducimus modi voluptate consequatur quas.
+              </p>
+            </motion.div>
+
+            <div className='relative mt-16'>
+              <div className='before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles'>
+                <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </div>
   )
